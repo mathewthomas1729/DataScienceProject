72c1ab5d2f8b5fb4fa856c387377f6465af11cb2
mathewthomas1729 <mathewthomas1729@gmail.com>
2016-12-05 21:53:56 -0500

    committing first python file

---
+++ calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# functions.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    testfunc()
+    print testfunc().__doc__
+def testfunc():
+    This is a test function
+    print('This is a test function')
+
+if __name__ == "__main__": main()
