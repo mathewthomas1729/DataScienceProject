08f57c40aad2ceb54fd465b6de517e5ebcff47ba
mathewthomas1729 <mathewthomas1729 @gmail.com>
2017-01-12 19:07:46 -0500

    adding folder for project

---
+++ EstimatingUberTraffic/GetUberData.sh
@@ -0,0 +1,4 @@
+curl -X GET -H  'Authorization: Token -l3XEhlZRPidhD3pbwMAT44BAn6W-9ZdWqlV9EJU' \
+     -H 'Accept-Language: en_US' \
+     -H 'Content-Type: application/json' \
+     'https://api.uber.com/v1.2/estimates/price?start_latitude=37.7752315&start_longitude=-122.418075&end_latitude=37.7752415&end_longitude=-122.518075'
