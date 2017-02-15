ab4388b51e603fc8695b088c6726fd4fd29d4448
mathewthomas1729 <mathewthomas1729 @gmail.com>
2017-02-15 07:46:05 -0500

    adding query file

---
+++ EstimatingUberTraffic/Dataset_Queries.sql
@@ -0,0 +1,14 @@
+--move dataset to data directory without headers;
+
+delete * from tweet_dataset;
+
+copy tweet_dataset from 'C:\Program Files\PostgreSQL\9.6\data\sample_streamed.txt' with delimiter E'\t';
+
+select * from tweet_dataset;
+
+select * from tweet_Dataset where tweet_text like '%traffic%';
+
+select * from tweet_Dataset where tweet_text like '%concert%';
+
+
+
